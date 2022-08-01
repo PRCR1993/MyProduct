fe6802e08fc73261ab88bf3073c6d1433313a2a5
ramachandra.patakota@gmail.com <PRCR1993>
2022-08-01 20:06:27 +0530

    First commit ti product

---
+++ ProductSML/src/main/java/com/realprograms/ProductSML/ProductSmlApplication.java
@@ -0,0 +1,13 @@
+package com.realprograms.ProductSML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSmlApplication.class, args);
+	}
+
+}
---
+++ ProductSML/src/test/java/com/realprograms/ProductSML/ProductSmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realprograms.ProductSML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
